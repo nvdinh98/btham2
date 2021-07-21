a53a7b69e4dab713c05634f50caf18b6230e0fee
nvdinh98 <86923066+nvdinh98@users.noreply.github.com>
2021-07-21 08:38:24 +0700

    update

---
+++ demo.js
@@ -0,0 +1,8 @@
+function  square(number) {
+    var result = number * number ;
+    return result;
+}
+var square0f2 = square(2);
+document.write("square of 2 is :" + square0f2);
+var square0f2 = square(5);
+document.write("binh phuong cua 5 la " + square(5));
